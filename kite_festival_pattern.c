a08b66707e437f22a2f9d3d504930f9b171ccc01
krrish savlani <savlanikrrish@gmail.comgit config --global user.email savlanikrrish@gmail.com>
2024-01-15 20:48:06 +0530

    full source code

--- kite_festival_pattern.c
+++ kite_festival_pattern.c
@@ -1,36 +1,48 @@
 #include <stdio.h>
 
 int main() {
-for(int i = 1 ; i<= 10 ; i++)
+for(int i = 1 ; i<= 15 ; i++)
 {
 
 
  for(int j = 5; j >= 1 ; j-- )
  {
  if(i == j){printf("*");}
- else {printf("    ");}
+ else {printf("     ");}
  }
-  printf ("   ");
+  printf ("   "); // this is giving a space to another matrix 
  for(int j = 2; j <= 5 ; j++)
  {
  if(i == j){printf("*");}
- else {printf("    ");}
+ else {printf("     ");}
  }
  
  printf("\n");
   for (int j= 5 ; j<= 9 ; j++)
  {
  if(i == j){printf("*");}
- else {printf("    ");}
+ else {printf("     ");}
  }
  
  printf("   ");
  for(int j = 8; j >= 5; j-- )
  {
  if(i == j){printf("*");}
- else {printf("    ");}
+ else {printf("     ");}
  }
  
+ printf("\n");
+ 
+ 
+ for(int j = 11 ; j<= 15 ; j++)
+ {printf ("  ");}
+ printf ("       ");
+ 
+ for (int j = 9 ; j<= 15 ; j++)
+ {
+     if (i==j){printf ("\\");}
+     else {printf (" ");}
+ }
  printf ("\n");
 }
     return 0;
